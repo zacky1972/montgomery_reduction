cb28c81d98275d01cef510ce64c1608e7d58260e
Susumu Yamazaki <zacky1972@gmail.com>
2025-07-18 03:39:23 +0900

    initial commit

---
+++ lib/montgomery_reduction.ex
@@ -0,0 +1,18 @@
+defmodule MontgomeryReduction do
+  @moduledoc """
+  Documentation for `MontgomeryReduction`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MontgomeryReduction.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/montgomery_reduction_test.exs
@@ -0,0 +1,8 @@
+defmodule MontgomeryReductionTest do
+  use ExUnit.Case
+  doctest MontgomeryReduction
+
+  test "greets the world" do
+    assert MontgomeryReduction.hello() == :world
+  end
+end
